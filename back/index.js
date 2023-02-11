dd7d5ad052c891b94bd86407b3758fc8986b3752
Khangalhngo <carbonskill1@gmail.com>
2023-02-11 16:08:09 +0800

    update user jo paaaraatai

--- back/index.js
+++ back/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const User = require("./models/user.model");
 const jwt = require("jsonwebtoken");
 const authenticateJWT = require("./middleware/index");
+const asynchHandler = require("express-async-handler");
 app.use(cors());
 app.use(express.json());
 
@@ -27,6 +28,21 @@ app.post("/api/register", async (req, res) => {
       email: req.body.email,
       password: req.body.password,
       role: req.body.role,
+      fname: req.body.fname,
+      lname: req.body.lname,
+      pnum1: req.body.pnum1,
+      pnum2: req.body.pnum2,
+      province: req.body.province,
+      bag: req.body.bag,
+      sum: req.body.sum,
+      delgerengui: req.body.delgerengui,
+      gender: req.body.gender,
+      surguuli: req.body.surguuli,
+      year: req.body.year,
+      day: req.body.day,
+      month: req.body.month,
+      angi: req.body.angi,
+      tovchtaniltsuulga: req.body.tovchtaniltsuulga,
     });
     res.json({ status: "ok" });
   } catch (error) {
@@ -74,6 +90,63 @@ app.post("/api/userData", authenticateJWT, async (req, res) => {
     res.json({ status: "error", error: "userData error" });
   }
 });
+
+app.post("/api/updateUserData", authenticateJWT, async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  console.log(req.user);
+
+  // try {
+  //   const useremail = req.body;
+  //   User.findOne({ email: useremail })
+  //     .then((data) => {
+  //       res.send({ status: "ok", data: data });
+  //     })
+  //     .catch((error) => {
+  //       res.send({ status: "error", data: error });
+  //     });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.json({ status: "error", error: "userData error" });
+  // }
+  // if (user) {
+  //   user.email = req.body.email || user.email;
+  //   const updateUser = await user.save();
+  //   res.json({
+  //     fname: updateUser.fname,
+  //     lname: updateUser.lname,
+  //   });
+  // } else {
+  //   res.status(401);
+  //   throw new Error("User Not found");
+  // }
+  User.findById(req.user.id, function (err, user) {
+    // todo: don't forget to handle err
+
+    if (!user) {
+      req.flash("error", "No account found");
+      // return res.redirect('/edit');
+    }
+
+    // good idea to trim
+    // var email = req.body.email.trim();
+    // var username = req.body.username.trim();
+    // var firstname = req.body.firstname.trim();
+    // var lastname = req.body.lastname.trim();
+    var fname = req.body.fname.trim();
+    var lname = req.body.fname.trim();
+
+    // validate
+
+    // no need for else since you are returning early ^
+    user.fname = fname;
+    user.lname = lname;
+
+    // don't forget to save!
+    user.save(function (err) {});
+  });
+});
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- back/models/user.model.js
+++ back/models/user.model.js
@@ -6,8 +6,27 @@ const User = new mongoose.Schema(
     password: { type: String, required: true },
     role: { type: String, required: true },
     quote: { type: String },
+    fname: { type: String },
+    lname: { type: String },
+    pnum1: { type: String },
+    pnum2: { type: String },
+    province: { type: String },
+    bag: { type: String },
+    sum: { type: String },
+    delgerengui: { type: String },
+    gender: { type: String },
+    surguuli: { type: String },
+
+    year: { type: String },
+    day: { type: String },
+    month: { type: String },
+    angi: { type: String },
+    tovchtaniltsuulga: { type: String },
   },
   { collection: "user-data" }
 );
+
 const model = mongoose.model("User", User);
+// const model2 = mongoose.model("UserDetails", UserDetails);
 module.exports = model;
+// module.exports = model2;
